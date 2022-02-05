1b5506db987f72bfa1aa95f3a78e31dc1e53af47
Sai-Sri-Haran <sai.sriharan@codingmart.com>
2022-02-05 23:07:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+import ProtectedRouter from "./Assests/ProtectedRouter";
 
+import Home from "./Pages/HomePage/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <ProtectedRouter path="/" component={Home} />
+    </Routes>
   );
 }
-
 export default App;
